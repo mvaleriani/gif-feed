10b097fc687093d154909fc0990b286b2cfd621e
mvaleriani <michaelvaleriani@gmail.com>
2018-10-26 11:04:31 -0700

    attempt webpack fix

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,11 +1,31 @@
 var path = require("path");
 var webpack = require('webpack');
 
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
 module.exports = {
   context: __dirname,
   entry: "./Gifinity.jsx",
   output: {
-    path: path.resolve(__dirname, './'),
+    path: path.resolve(__dirname),
     filename: "bundle.js"
   },
   module: {
@@ -24,7 +44,5 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx", "*"],
   },
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-  ]
+  plugins: plugins
 };
